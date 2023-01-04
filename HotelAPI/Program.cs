78d8e20d386de0d41fb0ebe4ed7f3a836b1ec1c3
Bereket Jacob <91929228+bjacobn@users.noreply.github.com>
2023-01-03 16:41:19 -0800

    Configure Serilog and Seq

--- HotelAPI/Program.cs
+++ HotelAPI/Program.cs
@@ -1,9 +1,9 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,7 +18,13 @@ builder.Services.AddCors(options =>
 });
 
 
+//Logger
+builder.Host.UseSerilog((ctx, lc) => 
+lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
