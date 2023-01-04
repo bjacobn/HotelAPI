98d8030ae01a98478ba5f84c43ea1e73d30ec8d5
Bereket Jacob <91929228+bjacobn@users.noreply.github.com>
2023-01-04 13:43:29 -0800

    Seeded countries and hotels data

--- HotelAPI/Data/HotelListDbContext.cs
+++ HotelAPI/Data/HotelListDbContext.cs
@@ -6,8 +6,73 @@ namespace HotelAPI.Data
     {
         public HotelListDbContext(DbContextOptions options) : base(options)
         {
-
+         
         }
 
-    }
+        //Entities
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        
+        //Seed Data Method
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            //Countries
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+               new Country
+               {
+                   Id = 2,
+                   Name = "Bahamas",
+                   ShortName = "BS"
+               },
+               new Country
+               {
+                   Id = 3,
+                   Name = "Cayman Island",
+                   ShortName = "CI"
+               }
+
+              );
+
+
+            //Hotels
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort",
+                    Address = "Negril",
+                    Rating = 4.5,
+                    CountryId = 1
+
+                },
+               new Hotel
+               {
+                   Id = 2,
+                   Name = "Comfort Suites",
+                   Address = "George Town",
+                   Rating = 4.3,
+                   CountryId = 3
+               },
+               new Hotel
+               {
+                   Id = 3,
+                   Name = "Grand Palldium ",
+                   Address = "Negril",
+                   Rating = 5,
+                   CountryId = 1
+               }
+
+             );
+        }
+    }   
 }
